0f146afc90d9de3c6402659b8dbff491ba792183
akash <akash300383@gmail.com>
2022-11-20 23:17:35 +0530

    done

--- App.java
+++ App.java
@@ -1,5 +1,6 @@
 public class App {
 	public static void main(String args[]) {
 		System.out.println("This is a simple job executed through jenkin");
+		System.out.println("another message added in java program");
 	}
 }
