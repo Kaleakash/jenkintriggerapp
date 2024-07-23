15f9c2702bc338e53e067f12340641b5367385f4
akash <akash300383@gmail.com>
2024-07-23 08:41:00 +0530

    jenkins file created

--- App.java
+++ App.java
@@ -3,6 +3,6 @@ public class App {
 		System.out.println("This is a simple job executed through jenkin");
 		System.out.println("another message added in java program");
 		System.out.println("another message added in java program again and again");
-		System.out.println("another message added in java program again and again")
+		System.out.println("another message added in java program again and again");
 	}
 }
